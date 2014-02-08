3a8fb48a0247b6dced7ba6eb25c678ce3943310b
donkey <tweegdong@gmail.com>
2014-02-08 13:24:08 +0800

    新上传了一个作品

--- js/handle.js
+++ js/handle.js
@@ -1,57 +1,57 @@
 $(function () {
-     //导航锚点
-    $('nav').on('click','a',function(e){
+    //导航锚点
+    $('nav').on('click', 'a', function (e) {
         e.preventDefault();
         var that = $(this);
         var indx = that.index();
-        $.scrollTo('#part'+ (indx + 1),600);
-        setTimeout(function(){  //todo 点击导航栏设置焦点目标为高亮 延迟执行
+        $.scrollTo('#part' + (indx + 1), 600);
+        setTimeout(function () {  //todo 点击导航栏设置焦点目标为高亮 延迟执行
             $('nav').find('.cur').removeClass('cur');
             that.addClass('cur');
-        },601)
+        }, 601)
 
     });
 
     //导航位置控制
     var hh = $('header').height();
     var vh = $.waypoints('viewportHeight');
-    $('.part1').waypoint(function(direction) {  //todo 让导航栏保持在底部
-        if(direction === 'down'){
-            $('header').css('position','static');
-        }else{
+    $('.part1').waypoint(function (direction) {  //todo 让导航栏保持在底部
+        if (direction === 'down') {
+            $('header').css('position', 'static');
+        } else {
             $('header').removeAttr('style');
         }
-    },{
-        offset:function(){
+    }, {
+        offset: function () {
             return vh - $(this).height() - hh;
         }
     });
-    $('.part2').waypoint(function(direction) {  //todo 让导航栏保持在顶端
-        if(direction === 'down'){
+    $('.part2').waypoint(function (direction) {  //todo 让导航栏保持在顶端
+        if (direction === 'down') {
             $('header').css({
                 'position': 'fixed',
-                'top':0,
-                'bottom':''
+                'top': 0,
+                'bottom': ''
             });
-        }else{
+        } else {
             $('header').css({
                 'position': 'static'
             });
         }
-    },{
-        offset:function(){
+    }, {
+        offset: function () {
             return hh;
         }
     });
 
     //滚动到栏目块位置设置导航显示对应高亮标签
-    $('.section').waypoint(function(direction){
+    $('.section').waypoint(function (direction) {
         //console.log($('.section').index(this));
         var a = $('nav').find('a');
         a.removeClass('cur');
         a.eq($('.section').index(this)).addClass('cur');
-    },{
-        offset:function(){
+    }, {
+        offset: function () {
             return -hh;
         }
     });
